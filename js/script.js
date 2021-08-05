3c8c1c3b47376a4fe9ba9a5d4e431a73acf300b1
Nikita Danilkin <ni.danilkin@gmail.com>
2021-08-06 01:18:23 +0300

    create: create and responsive news section

--- js/script.js
+++ js/script.js
@@ -1,28 +1,42 @@
-'use strict';
+"use strict";
 
 const tabs = () => {
-  const tabHeader = document.querySelector('.recommendations__list'),
-    tab = [...tabHeader.querySelectorAll('.recommendations__item')],
-    tabContent = document.querySelectorAll('.main-article__promo');
+  const tabHeader = document.querySelector(".recommendations__list"),
+    tab = [...tabHeader.querySelectorAll(".recommendations__item")],
+    tabContent = document.querySelectorAll(".main-article__promo");
 
-  const toggleTabContent = index => {
+  const toggleTabContent = (index) => {
     for (let i = 0; i < tabContent.length; i++) {
       if (index === i) {
-        tab[i].classList.add('recommendations__item--active');
-        tabContent[i].classList.add('main-article__promo--active');
+        tab[i].classList.add("recommendations__item--active");
+        tabContent[i].classList.add("main-article__promo--active");
       } else {
-        tab[i].classList.remove('recommendations__item--active');
-        tabContent[i].classList.remove('main-article__promo--active');
+        tab[i].classList.remove("recommendations__item--active");
+        tabContent[i].classList.remove("main-article__promo--active");
       }
     }
   };
 
-  tabHeader.addEventListener('click', event => {
+  tabHeader.addEventListener("click", (event) => {
     let target = event.target;
-    target = target.closest('.recommendations__item');
+    target = target.closest(".recommendations__item");
+    console.log(target);
     toggleTabContent(tab.indexOf(target));
-    
   });
 };
 
 tabs();
+
+const addToBookmarks = () => {
+  let bookmark = document.querySelectorAll(".bookmark__icon");
+
+  document.addEventListener("click", (event) => {
+    let target = event.target;
+
+    if (target.closest(".bookmark__icon")) {
+      target.classList.toggle("bookmark__icon--active");
+    }
+  });
+};
+
+addToBookmarks();
